ea51cddf2dec10f1ab03b1d2964b363b2fb7f258
KKherrazi <khalid.kherrazi@gmail.com>
2014-10-31 01:05:44 +0100

    Root directory chooser

--- FileManager/src/application/Main.java
+++ FileManager/src/application/Main.java
@@ -1,20 +1,74 @@
 package application;
 	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import java.io.File;
 
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	File directory;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			
+			primaryStage.setTitle("File Manager");
+	         
+	        final Label labelFile = new Label();
+	         
+	        Button btn = new Button();
+	        btn.setText("Choose root folder...");
+	        
+	        ListView<String> list = new ListView<String>();
+	        
+	        btn.setOnAction(new EventHandler<ActionEvent>() {
+	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	            	DirectoryChooser chooser = new DirectoryChooser();
+	                 
+	                //Open directory from existing directory
+	                if(directory != null){
+	                    File existDirectory = directory.getParentFile();
+	                    chooser.setInitialDirectory(existDirectory);
+	                }
+	 
+	                //Set extension filter
+	                //FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("AVI files (*.avi)", "*.avi");
+	                File defaultDirectory = new File("c:/");
+					chooser.setInitialDirectory(defaultDirectory);
+					
+				    //Show open file dialog
+					directory = chooser.showDialog(primaryStage);
+	                 
+	                labelFile.setText(directory.getPath());
+	                
+	                ObservableList<String> items = FXCollections.observableArrayList (directory.list());
+	                list.setItems(items);
+	            }
+	        });
+	         
+	        VBox vBox = new VBox();
+	        vBox.getChildren().addAll(labelFile, btn, list);
+	         
+	        StackPane root = new StackPane();
+	        root.getChildren().add(vBox);
+	        primaryStage.setScene(new Scene(root, 600, 650));
+	        primaryStage.show();
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
